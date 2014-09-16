3d2b9fc67c8b40760e5252701f21cd93e8058f35
ryanarmstrong <jenova49@gmail.com>
2014-09-16 16:47:49 -0700

    Added settings.php injection.

--- bin/inc/install.sh
+++ bin/inc/install.sh
@@ -20,8 +20,16 @@ install() {
     mysql --host=${mysql_host} --user=${mysql_user} --password=${mysql_password} -e "create database ${project_name};"
   fi
 
+  # Install Drupal
   cd ${webroot_dir}
   drush si ${project_name} --db-url=mysql://${mysql_user}:${mysql_password}@${mysql_host}/${project_name} --account-pass=${drupal_admin_password} --yes
+
+  # Add any custom settings
+  if [ ${profile_dir}/settings.php ]; then
+    chmod 777 ${webroot_dir}/sites/default/settings.php
+    cat ${profile_dir}/settings.php >> ${webroot_dir}/sites/default/settings.php
+    chmod 644 ${webroot_dir}/sites/default/settings.php
+  fi
 
   if [ "$development" == '1' ];
     then
---
+++ example.settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @file
+ * Drupal site-specific configuration file.
+ *
+ * If you need to add anything to the settings.php file, rename this file to settings.php and
+ * the installer will inject that upon site install.
+ */
